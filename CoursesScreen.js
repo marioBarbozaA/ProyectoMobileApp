11ccc13eefaf4b38ad1186891651430a726a52f3
Mario Barboza <90014477+marioBarbozaA@users.noreply.github.com>
2024-03-06 18:51:21 -0600

    Front end cooked

--- CoursesScreen.js
+++ CoursesScreen.js
@@ -1,12 +1,113 @@
-// CoursesScreen.js
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  Button,
+  Modal,
+  TextInput,
+} from "react-native";
+import PrincipalButton from "./components/PrincipalButton";
+
+// Mock de datos de cursos para el ejemplo
+const mockCourses = [
+  { id: "1", name: "Matemáticas", description: "Curso básico de matemáticas" },
+  {
+    id: "2",
+    name: "Historia",
+    description: "Introducción a la historia universal",
+  },
+  // Añade más cursos según necesites
+];
 
 const CoursesScreen = () => {
+  const [courses, setCourses] = useState(mockCourses); // Estado para la lista de cursos
+  const [modalVisible, setModalVisible] = useState(false); // Estado para la visibilidad del modal
+  const [currentCourse, setCurrentCourse] = useState(null); // Estado para el curso actual en edición
+
+  // Funciones para manejar el CRUD
+  const handleAddCourse = () => {
+    // Aquí lógica para añadir un nuevo curso
+    console.log("Añadir nuevo curso");
+    setModalVisible(true);
+    setCurrentCourse(null); // Limpiar el curso actual para el modal de añadir
+  };
+
+  const handleEditCourse = (course) => {
+    // Aquí lógica para editar un curso
+    console.log("Editar curso", course.id);
+    setModalVisible(true);
+    setCurrentCourse(course); // Establecer el curso actual para el modal de edición
+  };
+
+  const handleDeleteCourse = (courseId) => {
+    // Aquí lógica para eliminar un curso
+    console.log("Eliminar curso", courseId);
+    // Implementar la lógica para eliminar el curso del estado y del backend
+  };
+
+  // Render Item para FlatList
+  const renderCourse = ({ item }) => (
+    <View style={styles.courseItem}>
+      <Text style={styles.courseTitle}>{item.name}</Text>
+      <Text>{item.description}</Text>
+      <View style={styles.buttonsContainer}>
+        <PrincipalButton
+          title="Editar"
+          onPress={() => handleEditCourse(item)}
+        />
+        <PrincipalButton
+          title="Eliminar"
+          onPress={() => handleDeleteCourse(item.id)}
+          color="red"
+        />
+      </View>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Lista de Cursos</Text>
-      {/* Aquí agregarías la lógica para listar los cursos y las operaciones CRUD */}
+      <PrincipalButton title="Añadir Curso" onPress={handleAddCourse} />
+      <FlatList
+        data={courses}
+        renderItem={renderCourse}
+        keyExtractor={(item) => item.id}
+      />
+      {/* Modal para añadir/editar curso */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        {/* Contenido del modal */}
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <TextInput
+              placeholder="Nombre del Curso"
+              // Aquí lógica para manejar el nombre del curso
+              placeholderTextColor="black" // Esta línea cambia el color del placeholder a negro
+            />
+            <TextInput
+              placeholder="Descripción del Curso"
+              placeholderTextColor="black" // Esta línea cambia el color del placeholder a negro
+
+              // Aquí lógica para manejar la descripción del curso
+            />
+            <PrincipalButton
+              title="Guardar"
+              onPress={() => {
+                // Aquí lógica para guardar el curso
+                setModalVisible(!modalVisible);
+              }}
+            />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -14,9 +115,47 @@ const CoursesScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 50,
   },
+  courseItem: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  courseTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
+  },
+  // Estilos para el modal
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)", // Fondo semi-transparente
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    color: "black",
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+
+  // Puedes añadir más estilos según necesites
 });
 
 export default CoursesScreen;
--- StudentsScreen.js
+++ StudentsScreen.js
@@ -1,26 +1,196 @@
-// En un nuevo archivo, por ejemplo, StudentsScreen.js
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Button,
+  FlatList,
+  Modal,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import PrincipalButton from "./components/PrincipalButton";
+import { Picker } from "@react-native-picker/picker"; // Asegúrate de instalar esta librería si no está incluida en tu proyecto
 
-function StudentsScreen({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Text>Estudiantes</Text>
-      {/* Aquí iría la implementación de tus CRUD para estudiantes */}
-      <Button
-        title="Añadir Estudiante"
-        onPress={() => console.log("Añadir estudiante")}
-      />
+// Mock de datos de estudiantes para el ejemplo
+const mockCourses = [
+  { id: "1", name: "Matemáticas" },
+  { id: "2", name: "Literatura" },
+  { id: "3", name: "Ciencias" },
+  // ... otros cursos
+];
+
+const mockStudents = [
+  { id: "1", name: "Juan Perez", email: "juan.perez@example.com", courses: [] },
+  { id: "2", name: "Ana Gomez", email: "ana.gomez@example.com", courses: [] },
+  // ... otros estudiantes
+];
+const StudentsScreen = () => {
+  const [students, setStudents] = useState(mockStudents);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [currentStudent, setCurrentStudent] = useState({
+    name: "",
+    email: "",
+    courses: [],
+  });
+  const [selectedCourses, setSelectedCourses] = useState([]);
+
+  // Función para abrir el modal para añadir/editar un estudiante
+  const handleOpenStudentModal = (student) => {
+    setCurrentStudent(student);
+    setSelectedCourses(student.courses);
+    setModalVisible(true);
+  };
+
+  // Función para manejar la selección de cursos
+  const handleSelectCourse = (courseId) => {
+    setSelectedCourses((prevSelectedCourses) => {
+      if (prevSelectedCourses.includes(courseId)) {
+        // Si ya está seleccionado, lo eliminamos
+        return prevSelectedCourses.filter((id) => id !== courseId);
+      } else {
+        // Si no está seleccionado, lo agregamos
+        return [...prevSelectedCourses, courseId];
+      }
+    });
+  };
+
+  // Función para guardar los cambios del estudiante actual
+  const handleSaveStudent = () => {
+    // Aquí se debería enviar la información al backend y actualizar el estado
+    setModalVisible(false);
+  };
+  // Función para eliminar un estudiante
+  const handleDeleteStudent = (studentId) => {
+    // Aquí deberías también eliminar el estudiante del backend
+    setStudents((prevStudents) =>
+      prevStudents.filter((student) => student.id !== studentId)
+    );
+  };
+
+  // ...
+  // Render Item para FlatList
+  const renderStudentItem = ({ item }) => (
+    <View style={styles.studentItem}>
+      <Text style={styles.studentName}>{item.name}</Text>
+      <Text>{item.email}</Text>
+      <View style={{ flexDirection: "row" }}>
+        {/* Botón para editar */}
+        <PrincipalButton
+          onPress={() => handleOpenStudentModal(item)}
+          title="Editar"
+        />
+        {/* Botón para eliminar */}
+        <PrincipalButton
+          onPress={() => handleDeleteStudent(item.id)}
+          title="Borrar"
+        />
+      </View>
     </View>
   );
-}
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={students}
+        renderItem={renderStudentItem}
+        keyExtractor={(item) => item.id}
+      />
+      <PrincipalButton
+        title="Añadir Estudiante"
+        onPress={() =>
+          handleOpenStudentModal({ name: "", email: "", courses: [] })
+        }
+      />
+
+      {/* Modal para añadir/editar estudiantes */}
+      <Modal visible={modalVisible} animationType="slide" transparent={true}>
+        <View style={styles.modalView}>
+          <TextInput
+            style={styles.input}
+            placeholder="Nombre"
+            placeholderTextColor="black" // Esta línea cambia el color del placeholder a negro
+            value={currentStudent.name}
+            onChangeText={(text) =>
+              setCurrentStudent({ ...currentStudent, name: text })
+            }
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            placeholderTextColor="black" // Esta línea cambia el color del placeholder a negro
+            value={currentStudent.email}
+            onChangeText={(text) =>
+              setCurrentStudent({ ...currentStudent, email: text })
+            }
+          />
+
+          {/* Lista de cursos para seleccionar */}
+          <Text>Cursos:</Text>
+          {mockCourses.map((course) => (
+            <TouchableOpacity
+              key={course.id}
+              style={styles.courseItem}
+              onPress={() => handleSelectCourse(course.id)}
+            >
+              <Text style={{ marginRight: 10 }}>{course.name}</Text>
+              <Text>
+                {selectedCourses.includes(course.id)
+                  ? "Seleccionado"
+                  : "No Seleccionado"}
+              </Text>
+            </TouchableOpacity>
+          ))}
+
+          <PrincipalButton
+            title="Guardar Estudiante"
+            onPress={handleSaveStudent}
+          />
+        </View>
+      </Modal>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+  },
+  studentItem: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  studentName: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "gray",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 5,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
     alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  courseItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
   },
 });
-
 export default StudentsScreen;
